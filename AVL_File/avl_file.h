8260439fa0e90fe78a387b5bb8dc978a0802f296
Mariana Capuñay <mariana.capunay@utec.edu.pe>
2023-09-05 16:45:44 -0500

    metodos AVL

--- AVL_File/avl_file.h
+++ AVL_File/avl_file.h
@@ -1,7 +1,7 @@
 #ifndef AVLFILE_H
 #define AVLFILE_H
 #include "node.h"
-
+#include <vector>
 template <typename T>
 class AVLFile
 {
@@ -15,9 +15,48 @@ class AVLFile
     AVLFile();
     AVLFile(string file_name, string atributo);
     vector<long> search(T key); //devuelve posiciones de key en filename
+    vector<long> rangeSearch(T begin_key, T end_key); //devuelve posiciones de key en filename
     bool insert(T key);  //inserta key en el AVL y guarda nodo en heap_file
     bool remove(T key);  //elimina key del avl, cambia removed = true en filename, cambia el valor del nodo en heap_file (evaluar casos en que es hoja o no)
 
+    ~AVLFile();
 
+    private: //funciones recursivas
+    
 };
+
+template <typename T>
+AVLFile<T>::AVLFile(){
+    root = -1;
+}
+
+template <typename T>
+AVLFile<T>::AVLFile(string file_name, string atributo){
+    this->filename = file_name;
+    this->heap_file = "avl."+atributo+".bin";
+}
+
+template <typename T>
+vector <long> AVLFile<T>::search(T key){
+
+}
+
+template <typename T>
+vector <long> AVLFile<T>::rangeSearch(T begin_key, T end_key){
+
+}
+
+template <typename T>
+bool insert(T key){
+
+}
+
+template <typename T>
+bool remove(T key){
+
+}
+
+template <typename T>
+AVLFile<T>::~AVLFile(){}
+
 #endif
