9a7c9e3d34ed420f4bb3ada11ff608a26376630a
manyory <manyory.cueva@utec.edu.pe>
2023-09-08 10:04:44 -0500

    plantilla sequential

---
+++ Sequential_File/sequential_file.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+template <typename T>
+struct Record { // Estructura de un registro
+        T data;
+        long next_del;
+
+        Record() : next_del(-1) {}
+    };
+
+template <typename T>
+struct Header {// Estructura del encabezado
+        int header;
+    };
+
+
+template <typename T>
+class SequentialFile {
+private:
+    string file_name;
+    string heap_file_name;
+    string reorg_file_name;
+    fstream file;
+
+public:
+    SequentialFile();
+
+    SequentialFile(string file_name, string heap_file_name, string reorg_file_name);
+
+    void insert(const T& data);
+
+    vector<T> search(const T& key);
+
+    vector<T> rangeSearch(const T& begin_key, const T& end_key);
+
+    bool add(const T& data);
+
+    bool remove(const T& key);
+
+    void reorganize();
+
+    vector<T> load();
+
+
+    private: 
+    
+};
