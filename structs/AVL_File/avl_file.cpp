6f5d366802ad8f54c78dc4b94186bc1115cf1a77
JaimeRamosT <jaime.ramos@utec.edu.pe>
2023-09-24 02:02:28 -0500

    Errores detectados por QT corregidos

---
+++ structs/AVL_File/avl_file.cpp
@@ -0,0 +1,16 @@
+#include "avl_file.h"
+
+bool archivo_existe(const string& nombre){
+    ifstream archivo(nombre.c_str());
+    return archivo.good();
+}
+
+void crear_archivo(const string& nombre) {
+    ifstream archivo(nombre.c_str());
+    if (archivo.good()) { //si existe
+        archivo.close(); //cerrarlo
+        return; //ya no es necesario crear
+    }
+    ofstream file(nombre, ios::binary);
+    file.close();
+}
--- structs/AVL_File/avl_file.h
+++ structs/AVL_File/avl_file.h
@@ -8,20 +8,9 @@
 
 using namespace std;
 
-bool archivo_existe(const string& nombre){
-    ifstream archivo(nombre.c_str());
-    return archivo.good();
-}
+bool archivo_existe(const string& nombre);
 
-void crear_archivo(const string& nombre) {
-    ifstream archivo(nombre.c_str());
-    if (archivo.good()) { //si existe
-        archivo.close(); //cerrarlo
-        return; //ya no es necesario crear
-    }
-    ofstream file(nombre, ios::binary);
-    file.close();
-}
+void crear_archivo(const string& nombre);
 
 template <typename T>
 class AVLFile{
